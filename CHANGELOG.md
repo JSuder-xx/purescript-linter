5031ffeeff35f57d66179cef7dad32c79810cff8
JSuder-xx <j.j.suder@gmail.com>
2024-08-14 21:45:23 -0400

    Ridiculous IfThenElseLeftAlignedRule fix for chains

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,20 +1,24 @@
-### v0.1.0 (2024-8-6)
+### UNRELEASED
 
-- Initial release
+- Bug Fix: The IfThenElseLeftAligned rule did not handle chains of if/then/else.
+
+### v0.2.3 (2024-8-13)
+
+- Bug Fix: Expressions did not recurse into Application terms.
+
+### v0.2.2 (2024-8-8)
+
+- Bug fixes
+
+### v0.2.1 (2024-8-8)
+
+- Forgot to rebuild dist 🤦‍♂️
 
 ### v0.2.0 (2024-8-8)
 
 - Refactored Linter to Rule.
 - Setting process Exit Code so that CI fails appropriately.
 
-### v0.2.1 (2024-8-8)
+### v0.1.0 (2024-8-6)
 
-- Forgot to rebuild dist 🤦‍♂️
-
-### v0.2.2 (2024-8-8)
-
-- Bug fixes
-
-### v0.2.3 (2024-8-13)
-
-- Bug Fix: Expressions did not recurse into Application terms.
+- Initial release
--- src/Rule/IfThenElse.purs
+++ src/Rule/IfThenElse.purs
@@ -5,7 +5,6 @@ import Prelude
 import Data.Monoid (guard)
 import Rule (allExpressionsLintProducer)
 import Rule as Rule
-import PureScript.CST.SourceRange (leftAligned, sameLine)
 import PureScript.CST.Types (Expr(..))
 
 ifThenElseLeftAligned :: Rule.Rule
@@ -22,6 +21,18 @@ x =
           , """
 x = 
   if 1 == 2 then 3
+    else 4
+          """
+          , """
+x = 
+  if 1 == 2 
+       then 3
+    else 4
+          """
+          , """
+x = 
+    if 1 == 2 
+  then 3
     else 4
           """
           , """
@@ -43,16 +54,73 @@ x =
   then 3
   else 4
                 """
+          , """
+x = 
+  if 1 == 2 
+  then 3
+  else if 1 == 2
+  then 4
+  else 5
+                """
+          , """
+x = 
+  if 1 == 2 
+  then 3
+  else if 1 == 2
+  then 4
+  else if 1 == 2
+  then 5 
+  else 6
+                """
+          , """
+x = 
+  if 1 == 2 then 3
+  else if 1 == 2
+  then 4
+  else if 1 == 2 then 5 
+  else 6
+                """
+          , """
+x = 
+  if 1 == 2 then do
+    blee 
+    blah
+    bloo
+  else if 1 == 2 then do 
+    dingo
+    bingo
+    flamingo
+  else if 1 == 2 then do
+    sherbert
+    flerbert 
+  else do
+    bippity
+    boppity
+    boop
+                """
+
           ]
       }
   , lintProducer: allExpressionsLintProducer $ case _ of
-      ExprIf { keyword: { range: if' }, then: { range: then' }, else: { range: else' } } ->
-        ( guard (not $ (if' `sameLine` then') || (if' `leftAligned` then'))
-            $ pure { message: "When `if` and `then` are on separate lines they must be left aligned.", sourceRange: then' }
-        )
-          <>
-            ( guard (not $ (then' `sameLine` else') || (if' `leftAligned` else'))
-                $ pure { message: "When `then` and `else` are on separate lines then `else` must left align with `if`.", sourceRange: then' }
-            )
+      ExprIf { keyword: { range: { start: if' } }, then: { range: thenRange@{ start: then' } }, else: { range: elseRange@{ start: else' } } } ->
+        let
+          -- NOTE: This is a really horrific hack to handle the case when the if follows an else on the same line. 
+          -- Here is why
+          -- - It would be **easy** to recurse through if/then/else patterns while the else expression is another if/then/else, accumulating an array of if/then/else
+          --   and use that complete information to properly report.
+          -- - HOWEVER, that would only work for the top/first. The CST traversal machinery would then match each one of the child if/then/else's in turn and those would not
+          --   have the entire picture and so would fail. 
+          -- - To solve this problem would require either finer control over the visitation or, easier, would be some state/context that could be threaded through. This could hold
+          --   a set of already visited positions or something that would inform subsequent or child if/then/else expressions to skip/pass/ignore. If a few rules end up requiring this
+          --   context then I'll think about it, but for now this crappy-but-simple code should catch 99% of the bad code. 
+          isIfAligned x = if'.column == x.column || if'.column - 5 == x.column
+        in
+          ( guard (not $ (if'.line == then'.line) || (isIfAligned then'))
+              $ pure { message: "When `if` and `then` are on separate lines they must be left aligned.", sourceRange: thenRange }
+          )
+            <>
+              ( guard (not $ (then'.line == else'.line) || (isIfAligned else'))
+                  $ pure { message: "When `then` and `else` are on separate lines then `else` must left align with `if`.", sourceRange: elseRange }
+              )
       _ -> []
   }
